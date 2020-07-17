7028ae47a464149113446268c99021d42f4b8c5c
David Hill <dhill@mindcry.org>
2020-07-17 19:45:23 -0400

    fuzzers: update deps to latest master.

--- fuzzers/edwards/fuzz.go
+++ fuzzers/edwards/fuzz.go
@@ -2,31 +2,31 @@ package edwards
 
 import (
 	"bytes"
-	dcrd_chainec "github.com/decred/dcrd/chaincfg/chainec"
+	dcrd_edwards "github.com/decred/dcrd/dcrec/edwards/v2"
 )
 
 func Fuzz(input []byte) int {
-	dcrd_chainec.Edwards.ParsePubKey(input)
-	dcrd_chainec.Edwards.PrivKeyFromBytes(input)
+	dcrd_edwards.ParsePubKey(input)
+	dcrd_edwards.PrivKeyFromBytes(input)
 	{
-		priv, pub := dcrd_chainec.Edwards.PrivKeyFromScalar(input)
-		if priv != nil && pub != nil {
+		priv, pub, err := dcrd_edwards.PrivKeyFromScalar(input)
+		if err == nil {
 			hash := []byte{0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9}
-			r, s, err := dcrd_chainec.Edwards.Sign(priv, hash)
+			r, s, err := dcrd_edwards.Sign(priv, hash)
 			if err == nil {
-				sig := dcrd_chainec.Edwards.NewSignature(r, s)
-				if !dcrd_chainec.Edwards.Verify(pub, hash, sig.GetR(), sig.GetS()) {
-					panic("dcrd_chainec.Edwards.Verify")
+				sig := dcrd_edwards.NewSignature(r, s)
+				if !dcrd_edwards.Verify(pub, hash, sig.GetR(), sig.GetS()) {
+					panic("dcrd_edwards.Verify")
 				}
 				pub.Serialize()
 				serializedKey := priv.Serialize()
 				if !bytes.Equal(serializedKey, input) {
-					panic("dcrd_chainec.Edwards: key not equal")
+					panic("dcrd_edwards.Edwards: key not equal")
 				}
 			}
 		}
 	}
-	dcrd_chainec.Edwards.ParseDERSignature(input)
-	dcrd_chainec.Edwards.ParseSignature(input)
+	dcrd_edwards.ParseDERSignature(input)
+	dcrd_edwards.ParseSignature(input)
 	return 0
 }
